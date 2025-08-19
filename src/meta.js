5efbc245de0331178ab081da0d68d241611b2bd4
Martín (Netux) Rodríguez <send.email@netux.site>
2025-08-19 02:45:58 +0000

    chore(meta): add time the list of things the thermometer widget can display

--- src/meta.js
+++ src/meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Internet Roadtrip - Thermometer
-// @description  Display a thermometer widget with the temperature and humidity of the current location of the neal.fun/internet-roadtrip vehicle
+// @description  Display a thermometer widget with the temperature, time, and humidity of the current location of the neal.fun/internet-roadtrip vehicle
 // @namespace    me.netux.site/user-scripts/internet-roadtrip/thermometer
 // @icon         https://cloudy.netux.site/neal_internet_roadtrip/Thermometer%20logo.png
 // @match        https://neal.fun/*
